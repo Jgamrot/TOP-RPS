9b1f6647a14b20cd9f13c763cfbd482c7ec98f5e
JGamrot <145107244+Jgamrot@users.noreply.github.com>
2024-04-03 14:17:03 +0100

    Meet Project requirements
    Think I've finished at this stage. I've met
    the requirements. Got a bit stuck on the score
    realised I needed extra variables.

--- script.js
+++ script.js
@@ -2,8 +2,23 @@
 // Paper 1
 // Scissors 0
 
+let playerScore = 0;
+let computerScore = 0;
+
+for (let i = 0; i <5; i++){
+
 function getComputerChoice(){
     return (Math.random()*3)
+}
+
+function getPlayerChoice(playerValue){
+    if (playerValue === "rock") {
+        return (2);
+       } else if (playerValue === "paper") {
+      return (1);
+       } else if (playerValue === "scissors") {
+      return (0);
+    }
 }
 
 function playRound(playerValue, computerSelection) {
@@ -19,20 +34,26 @@ function playRound(playerValue, computerSelection) {
     if ((playerValue > 1) && (computerSelection > 1)){
         return ("so did you, it's a draw!")
     } else if ((playerValue > 1) && (computerSelection === 1)){
+        computerScore++
         return ("you lose")
     } else if ((playerValue > 1) && (computerSelection < 1)){
+        playerScore++
         return ("you win")
         //playerValue Paper
     } else if ((playerValue === 1) && (computerSelection > 1)){
+        playerScore++
         return ("you win")
     } else if ((playerValue === 1) && (computerSelection === 1)){
         return ("so did you, it's a draw!")
     } else if ((playerValue === 1) && (computerSelection < 1)){
+        computerScore++
         return ("you lose")
         //playerValue Scissors
     } else if ((playerValue < 1) && (computerSelection > 1)){
+        computerScore++
         return ("you lose")
     } else if ((playerValue < 1) && (computerSelection === 1)){
+        playerScore++
         return ("you win")
     } else if ((playerValue < 1) && (computerSelection < 1)){
         return ("so did you, it's a draw!")
@@ -47,19 +68,18 @@ function playRound(playerValue, computerSelection) {
         return ("Computer picks Scissors " + result());
     }
 }
-function getPlayerChoice(playerValue){
-    if (playerValue === "rock") {
-        return (2);
-       } else if (playerValue === "paper") {
-      return (1);
-       } else if (playerValue === "scissors") {
-      return (0);
-    }
-}
-  
+
   const playerValue = prompt("Rock, paper or scissors?").toLowerCase();
   const playerSelection = getPlayerChoice(playerValue);
   const computerSelection = Math.floor(getComputerChoice());
 
   console.log(playRound(playerSelection, computerSelection));
-  
+}
+
+if (playerScore > computerScore) {
+    console.log("You win the best of 5!");
+  } else if (playerScore < computerScore) {
+    console.log("Computer wins the best of 5!");
+  } else {
+    console.log("It's a draw in the best of 5!");
+  }
